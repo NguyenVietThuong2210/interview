798b20998488bb1f3b7c05bc724b6c48c697f5f3
AlexNguyen <nguyenvietthuong0922@gmail.com>
2025-12-23 19:50:11 +0700

    update

--- interview/scripts/entrypoint.sh
+++ interview/scripts/entrypoint.sh
@@ -16,5 +16,5 @@ python manage.py migrate --noinput
 echo "Collecting static files..."
 python manage.py collectstatic --noinput
 
-# echo "Starting Gunicorn..."
+echo "Starting Gunicorn..."
 exec "$@"
