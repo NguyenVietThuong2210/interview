7b14a7a84a63d0de6d088e91f6d1e2619d26e3cb
AlexNguyen <nguyenvietthuong0922@gmail.com>
2025-12-26 17:50:32 +0700

    update

--- shared/settings.py
+++ shared/settings.py
@@ -5,12 +5,12 @@ load_dotenv()
 
 # Redis Configuration
 REDIS_HOST = os.getenv('REDIS_HOST', default='redis')
-REDIS_PORT = os.getenv('REDIS_PORT', default=6379, cast=int)
-REDIS_DB = os.getenv('REDIS_DB', default=0, cast=int)
+REDIS_PORT = os.getenv('REDIS_PORT', default=6379)
+REDIS_DB = os.getenv('REDIS_DB', default=0)
 
 # RabbitMQ Configuration
 RABBITMQ_HOST = os.getenv('RABBITMQ_HOST', default='rabbitmq')
-RABBITMQ_PORT = os.getenv('RABBITMQ_PORT', default=5672, cast=int)
+RABBITMQ_PORT = os.getenv('RABBITMQ_PORT', default=5672)
 RABBITMQ_USER = os.getenv('RABBITMQ_USER', default='guest')
 RABBITMQ_PASS = os.getenv('RABBITMQ_PASS', default='guest')
 RABBITMQ_EXPORT_QUEUE = 'export_queue'
